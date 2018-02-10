630d2d8e3cb452ea1c60fb1f96d27764f5d3be60
Chaitanya Talnikar <talnikar@mit.edu>
2018-02-09 22:23:25 -0500

    completed save lv

--- apps/save_LV.py
+++ apps/save_LV.py
@@ -4,8 +4,18 @@ import sys
 case = sys.argv[1]
 N_homo = int(sys.argv[2])
 
+from adFVM.interface import SerialRunner
+
 # compute djds v.s. # segment
 cp = fds.checkpoint.load_last_checkpoint(case, N_homo)
 v = cp.lss.lyapunov_covariant_vectors()
-V = cp.V
-import pdb;pdb.set_trace()
+v = rollaxis(rollaxis(v, 2), 2)
+V = array(cp.V)
+CLV = dot(V.T, v[-1,:,:]).T
+
+base = case + '/../'
+runner = SerialRunner(base, 2.0, None, None, nProcs=16)
+#for i in range(N_homo):
+for i in range(0, 1):
+    print('writing clv', i, 'at', 10 + i)
+    runner.writeFields(CLV[i], base, 10.0 + i)
